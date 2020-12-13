94ed412a6cc03b5aca7bfe30517f7fb4d154db84
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-13 10:19:41 -0800

    [StarshipInfo.js] added return button

--- src/components/StarshipInfo.js
+++ src/components/StarshipInfo.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 export default function StarshipInfo(props) {
     console.log(props.shipInfo.location.state)
@@ -7,6 +8,9 @@ export default function StarshipInfo(props) {
             <h3>Name: </h3>{props.shipInfo.location.state.name}
             <br></br>
             <h3>Model: </h3>{props.shipInfo.location.state.model}
+            <br></br>
+            <br></br>
+            <Link to="/">Return</Link>
         </div>
     )
 } 
